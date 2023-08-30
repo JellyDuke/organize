944db8d2e68b0122bd244f3387e39ff40ebe7b9a
ParkLouis <73382228+JellyDuke@users.noreply.github.com>
2023-08-30 21:28:42 +0900

    Add files via upload

---
+++ 인천일보아카데미/91day/MovieProject/src/main/java/com/movieProject/Service/MovieService.java
@@ -0,0 +1,39 @@
+package com.movieProject.Service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.movieProject.dao.MovieDao;
+import com.movieProject.dto.Movie;
+
+@Service
+public class MovieService {
+	
+	@Autowired
+	private MovieDao mvdao;
+	
+	public ArrayList<Movie> getMovieList() {
+		System.out.println("SERVICE - getMovieList");
+		
+		ArrayList<Movie> rankMovieList = mvdao.selectRankList();
+		
+		return rankMovieList;
+	}
+
+	public Movie getDetailMovie(String mvcode) {
+		System.out.println("SERVICE - getDetailMovie");
+		
+		return mvdao.selectDetailMovie(mvcode);
+	}
+
+	public ArrayList<Movie> getMovieList2() {
+		System.out.println("SERVICE - getMovieList2");
+		
+		ArrayList<Movie> movieList = mvdao.selectMovieList();
+		
+		return movieList;
+	}
+
+}
